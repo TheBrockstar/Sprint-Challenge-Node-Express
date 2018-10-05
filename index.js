b297744f3cf1818d5a04af640ff4f1ad1a75486e
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-10-05 10:20:23 -0600

    Implement READ All Actions Endpoint which retrieves an array of actions and sends the array to the client.

--- index.js
+++ index.js
@@ -14,17 +14,43 @@ const server = express();
 // Implement Server-wide Middleware
 server.use(express.json(), cors(), morgan('combined'), helmet());
 
+
 ///// ===============- SERVER CRUD ENDPOINTS -===============
 
 /// ##### Error Messages #####
+// =====- Project Database Error Messages -=====
 const unableToGetProjectList = {errorMessage: "Unable to retrieve projects."}
 
+// =====- Action Database Error Messages -=====
+const unableToGetActionList = {errorMessage: "Unable to retrieve actions."}
+
+
+//// ==========- Project Database Endpoints -==========
+
 /// ##### READ All Projects Endpoint #####
+
 server.get('/api/projects', (request, response) => {
+
+    // Database Helper Promise Method
     projectDb.get()
     .then(projects => response.status(200).send(projects))
     .catch(() => response.status(500).send(unableToGetProjectList))
 });
+
+
+
+//// ==========- Action Database Endpoints -==========
+
+/// ##### READ All Actions Endpoint #####
+server.get('/api/actions', (request, response) => {
+
+    // Database Helper Promise Method
+    actionDb.get()
+    .then(actions => response.status(200).send(actions))
+    .catch(() => response.status(500).send(unableToGetActionList))
+});
+
+
 
 /// Server Port and Listen Method
 const port = 4242;
