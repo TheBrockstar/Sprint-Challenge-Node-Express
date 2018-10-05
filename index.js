37b0beb2652ef076db3ddf0b8bbef3533795db20
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-10-05 10:13:10 -0600

    Implement READ All Projects Endpoint which retrieves an array of Project objects and sends the array to the client.

--- index.js
+++ index.js
@@ -5,14 +5,26 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 
 // Database Helpers
-const actionDb = require('./data/helpers/actionModel');
 const projectDb = require('./data/helpers/projectModel');
+const actionDb = require('./data/helpers/actionModel');
 
 // Instantiate Server
 const server = express();
 
 // Implement Server-wide Middleware
 server.use(express.json(), cors(), morgan('combined'), helmet());
+
+///// ===============- SERVER CRUD ENDPOINTS -===============
+
+/// ##### Error Messages #####
+const unableToGetProjectList = {errorMessage: "Unable to retrieve projects."}
+
+/// ##### READ All Projects Endpoint #####
+server.get('/api/projects', (request, response) => {
+    projectDb.get()
+    .then(projects => response.status(200).send(projects))
+    .catch(() => response.status(500).send(unableToGetProjectList))
+});
 
 /// Server Port and Listen Method
 const port = 4242;
